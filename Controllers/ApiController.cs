cc15ccd5b7f3db7a95642a94b5e3c911a70ee3ef
hghw <89172440+hghw@users.noreply.github.com>
2023-07-27 16:39:45 +0700

    init

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,9 +1,12 @@
 ﻿using api_web.Controllers.Helpers;
 using api_web.Module.MainPage.Models;
 using Microsoft.AspNetCore.Mvc;
-using Dapper;
-using Dapper.FastCrud;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPI;
 
 namespace api_web.Controllers
 {
@@ -18,6 +21,47 @@ namespace api_web.Controllers
         public ApiController(EF_DataContext context)
         {
             _context = context;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("/security/createToken")]
+        public string Token(User user)
+        {
+            if (user.UserName == "joydip" && user.Password == "joydip123")
+            {
+                var issuer = ConfigurationManagerBuilder.AppSetting["Jwt:Issuer"];
+                var audience = ConfigurationManagerBuilder.AppSetting["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes
+                (ConfigurationManagerBuilder.AppSetting["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                var stringToken = tokenHandler.WriteToken(token);
+                return stringToken;
+            }
+            else
+            {
+                return "";
+            }
+
         }
 
         [HttpGet("list")]
---
+++ Models/ConfigModels/ConfigBuilder.cs
@@ -0,0 +1,11 @@
+﻿class ConfigurationManagerBuilder
+{
+    public static IConfiguration AppSetting
+    {
+        get;
+    }
+    static ConfigurationManagerBuilder()
+    {
+        AppSetting = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+    }
+}
